f0805cda683b2f890607de99bc402b439d8bcb49
davidgardiner <davidgardiner97@gmail.com>
2020-06-04 07:03:16 -0400

    Delete load_spice_kernels.m

--- matlab/load_spice_kernels.m
+++
@@ -1,8 +0,0 @@
-
-pth = '/Users/robynmw/Dropbox/CODE/mice';
-addpath( [pth,'/lib'])      
-addpath( [pth,'/src/mice']) 
-addpath( [pth,'/kernels'])
-
-cspice_furnsh( [pth,'/kernels/de438.bsp'] )
-cspice_furnsh( '/Users/robynmw/Dropbox/CODE/mice/kernels/naif0012.tls' )
